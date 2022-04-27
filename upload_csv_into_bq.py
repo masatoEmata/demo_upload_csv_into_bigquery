86cfb2fadcfec03ca42e49c714f818f64ffc2294
Masato Emata (江俣 真人) <51349646+masatoEmata@users.noreply.github.com>
2022-04-27 14:39:52 +0900

    Add files via upload

---
+++ upload_csv_into_bq.py
@@ -0,0 +1,11 @@
+import pandas as pd
+
+file_path = './sample_change.csv'
+
+df = pd.read_csv(file_path, encoding='shift-jis')
+print(df)
+
+dataset_id = 'demo_dataset'
+table_id = 'demo_table'
+
+df.to_gbq('{}.{}'.format(dataset_id, table_id), if_exists='replace')
